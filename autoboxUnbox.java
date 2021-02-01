db5e97a530b26cf291eb63b496ce8dad414544bf
Manan Malhotra <mananmalhotra80@gmail.com>
2021-02-01 23:11:54 +0530

    Autoboxing Unboxing

--- autoboxUnbox.java
+++ autoboxUnbox.java
@@ -3,10 +3,12 @@ class auto{
     public static void main(String[] args) {
         ArrayList<Integer> intArr = new ArrayList<Integer>();
         for (int i =0; i<10;i++){
-            intArr.add(i*(4*88));
+            intArr.add(i); //Autoboxing intArr.add(Integer.valueOf(i))
+        //    intArr.add(Integer.valueOf(i));
         }
         for (int i =0; i<10;i++){
-            System.out.print(intArr.get(i)+" ");
+            System.out.print(intArr.get(i)+" "); // intArr.get(i).intValue();
+            // System.out.println(intArr.get(i).intValue());
         }
         System.out.println();
         System.out.println(intArr.toString());
